cdc9f95eb60d007d9b883076dfff68a66b1cc8b7
ROMAIN ROUSSEY <roussey.romain@gmail.com>
2025-11-11 15:11:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-936290752-6c0ff",
+  "appId": "1:893272311888:web:789c101e80d010d3e3a955",
+  "apiKey": "AIzaSyCQhWE1dOKDtOK-YzmoUwFTE1qopkf1QvI",
+  "authDomain": "studio-936290752-6c0ff.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "893272311888"
+};
