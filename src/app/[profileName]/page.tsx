b53d612f03b5530e361c78818fd5aba8849ef688
ROMAIN ROUSSEY <roussey.romain@gmail.com>
2025-11-18 07:35:02 +0000

    TU RACCONTE N IMPORT QUI OUIS LE SOUCI ET QUE LE MINISITE PUBLI EST AN 4

--- src/app/[profileName]/page.tsx
+++ src/app/[profileName]/page.tsx
@@ -43,7 +43,7 @@ export default function CounselorPublicProfilePage() {
     return query(
         collection(firestore, 'users'), 
         where('publicProfileName', '==', profileName),
-        where('role', '==', 'superadmin')
+        where('role', '==', 'conseiller')
     );
   }, [firestore, profileName]);
 
