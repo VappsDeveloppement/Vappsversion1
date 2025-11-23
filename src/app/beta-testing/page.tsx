01112ed8ae3034922601a651f6a7ba73c6e1626d
ROMAIN ROUSSEY <roussey.romain@gmail.com>
2025-11-23 13:44:41 +0000

    Try fixing this error: `Runtime Error: Error: CardFooter is not defined.

--- src/app/beta-testing/page.tsx
+++ src/app/beta-testing/page.tsx
@@ -9,7 +9,7 @@ import { useCollection, useMemoFirebase, addDocumentNonBlocking } from '@/fireba
 import { collection, query } from 'firebase/firestore';
 import { useFirestore } from '@/firebase/provider';
 import { useToast } from '@/hooks/use-toast';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
