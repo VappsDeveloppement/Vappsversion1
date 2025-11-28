2d85c1da0a3e06ed144e387abcb5b2248be95b4c
ROMAIN ROUSSEY <roussey.romain@gmail.com>
2025-11-28 06:10:51 +0000

    c est l url qui n est pas boone elle tente toujours d aller recuper un f

--- src/app/dashboard/e-learning/page.tsx
+++ src/app/dashboard/e-learning/page.tsx
@@ -56,15 +56,6 @@ type TrainingModule = ModuleFormData & {
     counselorId: string;
 };
 
-const toBase64 = (file: File): Promise<string> =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result as string);
-    reader.onerror = (error) => reject(error);
-  });
-
-
 function ModuleManager() {
     const { user } = useUser();
     const firestore = useFirestore();
